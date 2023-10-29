b145f632ce7502be92e6eaf8a7b8b582837bafb8
Kunj-Modi <modi.kunj07@gmail.com>
2023-10-30 03:29:36 +0530

    respond back get data

--- Client/get.py
+++ Client/get.py
@@ -2,6 +2,6 @@ import requests
 
 endpoint = 'http://localhost:8000/'
 
-response = requests.get(endpoint)
+response = requests.get(endpoint, params={'abc': 1234}, json={'name': 'kunj', 'code': 1234})
 
 print(response.json())
--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
+import json
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 
 @api_view(['GET'])
-def hey(request):
-    data = {'name': 'kunj', 'code': 7183}
+def hey(request, *args, **kwargs):
+    print(args, kwargs)
+    body = request.body
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        print("error")
+    data['params'] = request.GET
     return Response(data)
